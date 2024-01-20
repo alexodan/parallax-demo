a66a7744cdc70c315a1668e5574f185372dd8117
Alexis Diaz <alexdan1993@gmail.com>
2024-01-20 20:28:24 -0300

    Link and reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./App.module.scss";
 import { Link, Route } from "wouter";
 import Parallax1 from "./parallax/first";
+import Parallax4 from "./parallax/fourth";
 
 const App = () => (
   <div className={styles.app}>
@@ -12,11 +13,16 @@ const App = () => (
             <Link className="link" to="/parallax/1">
               First challenge
             </Link>
+            <br />
+            <Link className="link" to="/parallax/4">
+              Fourth challenge
+            </Link>
           </div>
         );
       }}
     />
     <Route path="/parallax/1" component={() => <Parallax1 />} />
+    <Route path="/parallax/4" component={() => <Parallax4 />} />
   </div>
 );
 
