a7454965afcff382d715420ec1a80661d1bcd1b6
Alexis Diaz <alexdan1993@gmail.com>
2024-01-23 01:00:51 -0300

    Using refs for intersection observer and class toggle

--- src/parallax/fourth/sections/two.tsx
+++ src/parallax/fourth/sections/two.tsx
@@ -1,49 +1,72 @@
-import { useEffect } from "react";
 import "./two.scss";
+import { useEffect, useRef, useState } from "react";
 
 const Section2 = () => {
+  const topLeft = useRef<HTMLDivElement>(null);
+  const topRight = useRef<HTMLDivElement>(null);
+  const bottomLeft = useRef<HTMLDivElement>(null);
+  const bottomRight = useRef<HTMLDivElement>(null);
+  const [elementsInViewport, setElementsInViewport] = useState({
+    "top-left": false,
+    "top-right": false,
+    "bottom-left": false,
+    "bottom-right": false,
+  });
+  const observerRef = useRef<IntersectionObserver | null>(null);
+
   useEffect(() => {
     const observerOptions = {
       root: null,
       rootMargin: "0px 0px 0px 0px",
+      threshold: 0.5,
     };
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-            // observer.unobserve(entry.target);
-          } else {
-            entry.target.classList.remove("show");
-          }
-        });
-      },
-      {
-        ...observerOptions,
-        threshold: 0.8,
-        // threshold: threshold / 100,
-      }
-    );
+    observerRef.current = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const target = entry.target as HTMLElement;
+        setElementsInViewport((prev) => ({
+          ...prev,
+          [target.dataset["id"]!]: entry.isIntersecting,
+        }));
+      });
+    }, observerOptions);
 
-    const topLeft = document.querySelector(`.top-left`) as HTMLElement;
-    const topRight = document.querySelector(`.top-right`) as HTMLElement;
-    observer.observe(topLeft);
-    observer.observe(topRight);
-  }, []);
+    observerRef.current.observe(topLeft.current!);
+    observerRef.current.observe(topRight.current!);
+    observerRef.current.observe(bottomLeft.current!);
+    observerRef.current.observe(bottomRight.current!);
+
+    return () => {
+      observerRef.current?.disconnect();
+    };
+  }, [topLeft, topRight, bottomLeft, bottomRight]);
 
   return (
     <section className="two">
       <div className="top">
-        <div className="top-left fade-in">
+        <div
+          ref={topLeft}
+          className={`top-left fade-in ${
+            elementsInViewport["top-left"] ? "show" : ""
+          }`}
+          data-id="top-left"
+        >
           <img loading="lazy" src="/assets/p2/left2.jpg" />
         </div>
-        <div className="top-right fade-in-slower">
-          <h2>
+        <div ref={topRight} data-id="top-right">
+          <h2
+            className={`top-right fade-in ${
+              elementsInViewport["top-right"] ? "show" : ""
+            }`}
+          >
             Air <br />
             balloons <br />
             festivals
           </h2>
-          <p>
+          <p
+            className={`top-right text fade-in ${
+              elementsInViewport["top-right"] ? "show" : ""
+            }`}
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
             rerum minima illo fuga, beatae sunt voluptates consequatur totam
             laudantium.
@@ -51,19 +74,33 @@ const Section2 = () => {
         </div>
       </div>
       <div className="bottom">
-        <div className="bottom-left">
-          <h2>
+        <div ref={bottomLeft} data-id="bottom-left">
+          <h2
+            className={`bottom-left fade-in ${
+              elementsInViewport["bottom-left"] ? "show" : ""
+            }`}
+          >
             About <br />
             air balloons
             <br /> safety
           </h2>
-          <p>
+          <p
+            className={`bottom-left text fade-in ${
+              elementsInViewport["bottom-left"] ? "show" : ""
+            }`}
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
             rerum minima illo fuga, beatae sunt voluptates consequatur totam
             laudantium.
           </p>
         </div>
-        <div className="bottom-right">
+        <div
+          ref={bottomRight}
+          className={`bottom-right fade-in ${
+            elementsInViewport["bottom-right"] ? "show" : ""
+          }`}
+          data-id="bottom-right"
+        >
           <img loading="lazy" src="/assets/p2/right2.jpg" />
         </div>
       </div>
